3487e2713ee9b37e7f84ff32dfd89034b9bcb7f3
marku <markusharen@gmail.com>
2021-11-11 16:42:21 +0100

    Styling ;)

--- src/main/java/com/example/demo/FacadeClass.java
+++ src/main/java/com/example/demo/FacadeClass.java
@@ -10,7 +10,9 @@ public class FacadeClass {
     public String getRandomString() {
         try {
             int quoteId = number.randomQuoteNr(jsReader.getAllQuotes().length);
-            return jsReader.getAllQuotes()[quoteId].quote;
+            String yes = jsReader.getAllQuotes()[quoteId].quote_id + ".\t " + jsReader.getAllQuotes()[quoteId].quote + "\t Quote from: " +
+                    jsReader.getAllQuotes()[quoteId].series + "\t said by: " + jsReader.getAllQuotes()[quoteId].author;
+            return yes;
         } catch (Exception e) {
             e.printStackTrace();
         }
